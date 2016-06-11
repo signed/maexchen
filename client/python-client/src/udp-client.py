7fa0a33e95f9dba78f0deaf69374a533d9ee142c
Jannek Link <linkjannek@gmail.com>
2016-06-11 11:52:24 +0200

    sende funktion hinzugefügt

--- client/python-client/src/udp-client.py
+++ client/python-client/src/udp-client.py
@@ -5,14 +5,19 @@ class Maexchen_Client:
     def __init__(self, server_ip="127.0.0.1", server_port=9000):
         self.server_port = server_port
         self.server_ip = server_ip
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     def echo(self, nachricht):
         nachricht_kommando = "ECHO;" + nachricht
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.sendto(nachricht_kommando.encode('utf-8'), (self.server_ip, self.server_port))
-        data, addr = sock.recvfrom(1024)
-        return (data.decode('utf-8'))
+        return self.sende(nachricht_kommando)
 
+    def beitreten(self,name):
+        pass
+
+    def sende(self,nachricht):
+        self.sock.sendto(nachricht.encode('utf-8'), (self.server_ip, self.server_port))
+        data, addr = self.sock.recvfrom(1024)
+        return (data.decode('utf-8'))
 
 normaler_client = Maexchen_Client()
 
