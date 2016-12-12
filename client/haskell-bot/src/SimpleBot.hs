2e4085024a05220fcade083abb7e092a9dbe7c29
Nicole Rauch <nicole.m@gmx.de>
2016-12-12 12:33:26 +0100

    Removed quotes around debug output

--- client/haskell-bot/src/SimpleBot.hs
+++ client/haskell-bot/src/SimpleBot.hs
@@ -7,7 +7,7 @@ import Network.Socket hiding (send, recvFrom)
 import Network.Socket.ByteString (send, recvFrom)
 
 import Data.List (isPrefixOf)
-import Data.ByteString.Char8 (split, pack)
+import qualified Data.ByteString.Char8 as BSC
 import Data.Char (digitToInt)
 import qualified Data.ByteString as BS
 
@@ -17,20 +17,20 @@ handler sock = do
     (msg,_) <- recvFrom sock 1024
     -- putStrLn $ "< " ++ msg
     let response = replyFor msg
-    print response
+    BSC.putStrLn response
     res <- if (BS.null response) then return 0 else send sock response
     handler sock
 
 register :: String -> BS.ByteString
-register teamname = pack $ "REGISTER;" ++ teamname
+register teamname = BSC.pack $ "REGISTER;" ++ teamname
 
 replyFor :: BS.ByteString -> BS.ByteString
 replyFor msg
-  | BS.isPrefixOf (pack "ROUND STARTING") msg = messageWithToken "JOIN;" msg
-  | BS.isPrefixOf (pack "YOUR TURN") msg = messageWithToken "SEE;" msg
+  | BS.isPrefixOf (BSC.pack "ROUND STARTING") msg = messageWithToken "JOIN;" msg
+  | BS.isPrefixOf (BSC.pack "YOUR TURN") msg = messageWithToken "SEE;" msg
   | otherwise = BS.empty
 
 
 messageWithToken :: String -> BS.ByteString -> BS.ByteString
-messageWithToken newMessage oldMessage = let token = head $ tail $ split ';' oldMessage
-                                                     in BS.append (pack newMessage) token
+messageWithToken newMessage oldMessage = let token = head $ tail $ BSC.split ';' oldMessage
+                                                     in BS.append (BSC.pack newMessage) token
